68f7ad739e9fdd879ac52ca09c0a8582e8eaf00d
dave6892 <dlu-dev@proton.me>
2025-04-04 10:33:12 -0400

    defined custom errors and added parameters to APIConfig

--- src/cpln/api/config.py
+++ src/cpln/api/config.py
@@ -1,10 +1,19 @@
-from dataclasses import dataclass, asdict
+from dataclasses import (
+    dataclass,
+    asdict
+)
+from ..constants import (
+    DEFAULT_CPLN_API_VERSION,
+    DEFAULT_TIMEOUT_SECONDS
+)
 
 @dataclass
 class APIConfig:
     base_url: str
     token: str
     org: str
+    version: str = DEFAULT_CPLN_API_VERSION
+    timeout: int = DEFAULT_TIMEOUT_SECONDS
     org_url: str = None
 
     def __post_init__(self):
