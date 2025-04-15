47dfec8ddd5035226dbbb18677f988bb8f8bf131
Dave L <9095628+dave6892@users.noreply.github.com>
2025-04-15 15:27:39 -0400

    update version (#10)

--- src/cpln/__init__.py
+++ src/cpln/__init__.py
@@ -4,4 +4,4 @@ from .client import (
 )
 from .api.client import APIClient
 from .version import get_version
-__version__ = "0.1.13"  # This version will be read by PDM
+__version__ = "0.1.14"  # This version will be read by PDM
